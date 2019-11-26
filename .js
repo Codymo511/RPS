e073b5471e8f596a295ec1ae7086c0e07f235aa1
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2019-11-26 10:52:18 -0500

    Update .js

--- .js
+++ .js
@@ -1,9 +1,15 @@
-function computerPlay(items)
-{
-  
-return items[Math.floor(Math.random()*items.length)];
-     
-}
+function computerPlay() {
+    const shapes = ['Rock', 'Paper', 'Scissors'];
+    return shapes[ Math.floor(Math.random() * 3) ];
+    }
 
-var items = ["rock","paper","scissors"];
-console.log(random_item(items));
+if (playerSelection == 'Rock' && computerSelection == 'Scissors'
+            || playerSelection == 'Paper' && computerSelection == 'Rock'
+            || playerSelection == 'Scissors' && computerSelection == 'Paper') {
+            round.textContent = 'You win! ' + playerSelection + ' beats ' + computerSelection + '.';
+            playerScore++; //Player wins.
+        } else {
+            round.textContent = 'You lose! ' + playerSelection + ' beaten by ' + computerSelection + '.';
+            computerScore++; //computer wins
+            }
+        }
