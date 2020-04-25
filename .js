be89dc728fe8e5837f0bf68ec229722796e785df
Codymo511 <55954712+Codymo511@users.noreply.github.com>
2020-04-25 15:47:28 -0400

    update functions

--- .js
+++ .js
@@ -1,23 +1,4 @@
-function computerPlay() {
-    const shapes = ['Rock', 'Paper', 'Scissors'];
-    return shapes[ Math.floor(Math.random() * 3) ];
-    }
-      
-function playRPS(computerSelection, playerSelection) {
-  // player selection is set to the buttons id.     
-  i++
-  if (i < 6) {
-    if (playerSelection === computerSelection) {       // Game tied.
-        round.textContent = 'Game tied! Both players chose ' + playerSelection + '.';
+const playOptions= ["rock","paper","scissors"];
 
-    } else {
-        if (playerSelection == 'Rock' && computerSelection == 'Scissors'
-            || playerSelection == 'Paper' && computerSelection == 'Rock'
-            || playerSelection == 'Scissors' && computerSelection == 'Paper') {
-            round.textContent = 'You win! ' + playerSelection + ' beats ' + computerSelection + '.';
-            playerScore++; //Player wins.
-        } else {
-            round.textContent = 'You lose! ' + playerSelection + ' beaten by ' + computerSelection + '.';
-            computerScore++; //computer wins
-            }
-        }
+let computerPlay = () => playOptions[Math.floor(Math.random() * playOptions.length)];
+
